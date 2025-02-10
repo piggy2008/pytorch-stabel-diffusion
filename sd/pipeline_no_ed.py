a30522df9e352514e895c236267df55f6addfb25
piggy2008 <piggy2008>
2025-02-10 11:11:21 +0900

    add cdprs testing code

--- sd/pipeline_no_ed.py
+++ sd/pipeline_no_ed.py
@@ -323,6 +323,191 @@ def generate_all(
         to_idle(diffusion)
         # to_idle(decoder)
 
+def generate_all_cdprs(
+        prompt,
+        uncond_prompt=None,
+        input_image_root=None,
+        image_path='',
+        save_root='',
+        strength=0.8,
+        do_cfg=True,
+        cfg_scale=7.5,
+        sampler_name="ddpm",
+        model_name='DiT',
+        n_inference_steps=50,
+        timestamps=2000,
+        models={},
+        seed=None,
+        device=None,
+        idle_device=None,
+        tokenizer=None,
+        gates_pretrained=''
+):
+    with (torch.no_grad()):
+        if not 0 < strength <= 1:
+            raise ValueError("strength must be between 0 and 1")
+
+        if idle_device:
+            to_idle = lambda x: x.to(idle_device)
+        else:
+            to_idle = lambda x: x
+
+        # Initialize random number generator according to the seed specified
+        generator = torch.Generator(device=device)
+        if seed is None:
+            generator.seed()
+        else:
+            generator.manual_seed(seed)
+
+        clip = models["clip"]
+        clip.to(device)
+
+        if do_cfg:
+            # Convert into a list of length Seq_Len=77
+            cond_tokens = tokenizer.batch_encode_plus(
+                [prompt], padding="max_length", max_length=77
+            ).input_ids
+            # (Batch_Size, Seq_Len)
+            cond_tokens = torch.tensor(cond_tokens, dtype=torch.long, device=device)
+            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            cond_context = clip(cond_tokens)
+            # Convert into a list of length Seq_Len=77
+            uncond_tokens = tokenizer.batch_encode_plus(
+                [uncond_prompt], padding="max_length", max_length=77
+            ).input_ids
+            # (Batch_Size, Seq_Len)
+            uncond_tokens = torch.tensor(uncond_tokens, dtype=torch.long, device=device)
+            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            uncond_context = clip(uncond_tokens)
+            # (Batch_Size, Seq_Len, Dim) + (Batch_Size, Seq_Len, Dim) -> (2 * Batch_Size, Seq_Len, Dim)
+            context = torch.cat([cond_context, uncond_context])
+        else:
+            # Convert into a list of length Seq_Len=77
+            tokens = tokenizer.batch_encode_plus(
+                [uncond_prompt], padding="max_length", max_length=77
+            ).input_ids
+            # (Batch_Size, Seq_Len)
+            tokens = torch.tensor(tokens, dtype=torch.long, device=device)
+            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            context = clip(tokens)
+        to_idle(clip)
+
+        if sampler_name == 'ddpm':
+            sampler = DDPMSampler(generator, num_training_steps=timestamps)
+            sampler.set_inference_timesteps(n_inference_steps)
+        elif sampler_name == 'rf':
+            sampler = RectifiedFlow()
+        else:
+            raise ValueError("Unknown sampler value %s. ")
+
+        latents_shape = (1, 4, LATENTS_HEIGHT, LATENTS_WIDTH)
+        diffusion = models["diffusion"]
+        diffusion.to(device)
+        layers_to_gate = range(0, 8)  # Specify layers to gate
+        gamma = 0.05
+        dgr = DistillationGuidedRouting(diffusion, layers_to_gate, device)
+        saved_gates = torch.load(gates_pretrained)
+        for layer, gate in saved_gates.items():
+            dgr.gates[layer].lambdas.data = gate.to(device)
+        print("Control gates loaded for inference.")
+        dgr.model.eval()
+        # encoder = models["encoder"]
+        # encoder.to(device)
+        # decoder = models["decoder"]
+        # decoder.to(device)
+        for image_name in image_path:
+            image = Image.open(os.path.join(input_image_root, image_name))
+
+            input_image_tensor = image.resize((WIDTH, HEIGHT))
+            # (Height, Width, Channel)
+            input_image_tensor = np.array(input_image_tensor)
+            # (Height, Width, Channel) -> (Height, Width, Channel)
+            input_image_tensor = torch.tensor(input_image_tensor, dtype=torch.float32, device=device)
+            # (Height, Width, Channel) -> (Height, Width, Channel)
+            input_image_tensor = rescale(input_image_tensor, (0, 255), (-1, 1))
+            # (Height, Width, Channel) -> (Batch_Size, Height, Width, Channel)
+            input_image_tensor = input_image_tensor.unsqueeze(0)
+            # (Batch_Size, Height, Width, Channel) -> (Batch_Size, Channel, Height, Width)
+            input_image_tensor = input_image_tensor.permute(0, 3, 1, 2)
+
+            # (Batch_Size, 4, Latents_Height, Latents_Width)
+            # encoder_noise = torch.randn(latents_shape, generator=generator, device=device)
+            # (Batch_Size, 4, Latents_Height, Latents_Width)
+            # condition = encoder(input_image_tensor, encoder_noise)
+
+            # Add noise to the latents (the encoded input image)
+            # (Batch_Size, 4, Latents_Height, Latents_Width)
+            # sampler.set_strength(strength=strength)
+            # latents = sampler.add_noise(latents, sampler.timesteps[0])
+
+            latents = torch.randn(input_image_tensor.shape, generator=generator, device=device)
+            if sampler_name == 'ddpm':
+                timesteps = tqdm(sampler.timesteps)
+                for i, timestep in enumerate(timesteps):
+                    # (1, 320)
+                    if model_name == 'DiT':
+                        time_embedding = torch.tensor([timestep]).to(device)
+                    else:
+                        time_embedding = get_time_embedding(timestep, 'test').to(device)
+                    # (Batch_Size, 4, Latents_Height, Latents_Width)
+                    model_input = torch.cat([input_image_tensor, latents], dim=1)
+
+                    if do_cfg:
+                        # (Batch_Size, 4, Latents_Height, Latents_Width) -> (2 * Batch_Size, 4, Latents_Height, Latents_Width)
+                        model_input = model_input.repeat(2, 1, 1, 1)
+
+                    # model_output is the predicted noise
+                    # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 4, Latents_Height, Latents_Width)
+                    model_output = dgr.model(model_input, context, time_embedding)
+
+                    if do_cfg:
+                        output_cond, output_uncond = model_output.chunk(2)
+                        model_output = cfg_scale * (output_cond - output_uncond) + output_uncond
+
+                    # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 4, Latents_Height, Latents_Width)
+
+                    latents = sampler.step(timestep, latents, model_output)
+
+            else:
+                # timesteps = tqdm(torch.from_numpy(np.arange(0, n_inference_steps)[::-1].copy()))
+                d_step = 1.0 / n_inference_steps
+
+                for j in tqdm(range(n_inference_steps)):
+                    if model_name == 'DiT':
+                        # print('j=', j)
+                        time_embedding = torch.tensor([j * d_step * timestamps]).to(device)
+                    else:
+                        time_embedding = get_time_embedding(timestep, 'test').to(device)
+                        # (Batch_Size, 4, Latents_Height, Latents_Width)
+                    model_input = torch.cat([input_image_tensor, latents], dim=1)
+
+                    if do_cfg:
+                        model_input = model_input.repeat(2, 1, 1, 1)
+
+                    # model_output is the predicted noise
+                    # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 4, Latents_Height, Latents_Width)
+                    model_output = diffusion(model_input, context, time_embedding)
+                    # output_cond = diffusion(model_input, context, time_embedding)
+                    # output_uncond = diffusion(model_input, context[1].unsqueeze(0), time_embedding)
+                    if do_cfg:
+                        output_cond, output_uncond = model_output.chunk(2)
+                        model_output = cfg_scale * (output_cond - output_uncond) + output_uncond
+
+                    # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 4, Latents_Height, Latents_Width)
+                    latents = sampler.euler(latents, model_output, d_step)
+            # (Batch_Size, 4, Latents_Height, Latents_Width) -> (Batch_Size, 3, Height, Width)
+            # images = decoder(latents)
+            images = rescale(latents, (-1, 1), (0, 255), clamp=True)
+            # (Batch_Size, Channel, Height, Width) -> (Batch_Size, Height, Width, Channel)
+            images = images.permute(0, 2, 3, 1)
+            images = images.to("cpu", torch.uint8).numpy()
+            if not os.path.exists(save_root):
+                os.makedirs(save_root)
+            Image.fromarray(images[0]).save(os.path.join(save_root, image_name))
+
+        # to_idle(encoder)
+        to_idle(diffusion)
+        # to_idle(decoder)
 
 def train(sampler_name="ddpm",
           prompt='',
